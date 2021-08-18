3df7816d67e2a4b4731fa58c8571ce618325718a
Ignacio Zamboni <ignacio.zamboni20@gmail.com>
2021-08-18 18:13:15 -0300

    pre-expand

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
+import {madd, neve, ives, seren, derok} from './resources/campaigs/HeartOfTyre';
+import {ahst, eustace, moe, raost, toot} from './resources/campaigs/ShadowOfTyre';
+import {bela, braktor, alexander, hackne, ozzie} from './resources/campaigs/SoulOfTyre';
 
 function Main() {
     return (
         <div className="main">
-            <Box campaign='Heart of Tyre' members={['Madd Morc', 'Neve', 'Ives Brightburn', 'Seren Thistlehoove', 'Lord Antonius Volnaris']} />
-            <Box campaign='Shadow of Tyre' members={['Ahst', 'Eustace', 'Moe Kowbull', 'Raost Tredder', 'Toot']} />
+            <Box campaign='Heart of Tyre' members={[madd, neve, ives, seren, derok]} />
+            <Box campaign='Shadow of Tyre' members={[ahst, eustace, moe, raost, toot]} />
+            <Box campaign='Soul of Tyre' members={[bela, braktor, alexander, hackne, ozzie]} />
 
         </div>
     );
@@ -18,8 +22,8 @@ function Box (props){
     }
 
     return (
-        <div className={'box'} onClick={changeActive}>
-            <Campaign campaign={props.campaign} />
+        <div className={'box'} /*onClick={changeActive}*/>
+            <Campaign campaign={props.campaign} actived={changeActive} />
             <Members members={props.members} active={active} />
         </div>
     );
@@ -27,7 +31,7 @@ function Box (props){
 
 function Campaign (props){
     return (
-        <div className='campaign'>    
+        <div className='campaign' onClick={props.actived}>    
             {props.campaign}
         </div>
     );
@@ -37,7 +41,14 @@ function Members (props) {
     return (
         <div className={'members '  + props.active}>
             <div className={"list-members "  + props.active}>
-                {props.members.map(member => (<div>{member}</div>))}
+                {props.members.map(member => (
+                    <div>
+                        <h4>{member.name}</h4> 
+                        <h5>Race: {member.race}</h5> 
+                        <h5>Class: {member.job}</h5>
+                        <h6><a href={member.wiki} target="_blank">More Info</a></h6>
+                    </div>
+                ))}
             </div>
         </div>
     );
---
+++ src/resources/campaigs/HeartOfTyre.js
@@ -0,0 +1,34 @@
+export const madd = {
+    name: 'Madd Morc',
+    race: 'Half-Orc',
+    job: 'Fighter',
+    wiki: 'https://verum.fandom.com/wiki/Madd_Morc'
+};
+
+export const neve = {
+    name: 'Neve',
+    race: 'Changeling',
+    job: 'Rogue',
+    wiki: 'https://verum.fandom.com/wiki/Neve'
+};
+
+export const ives = {
+    name: 'Ives Brightburn',
+    race: 'Human',
+    job: 'Rogue',
+    wiki: 'https://verum.fandom.com/wiki/Ives_Brightburn'
+};
+
+export const seren = {
+    name: 'Seren Tredder',
+    race: 'Eldar',
+    job: 'Dream Druid',
+    wiki: 'https://verum.fandom.com/wiki/Seren_Tredder'
+};
+
+export const derok = {
+    name: 'Derok Dranf',
+    race: 'Vega',
+    job: 'Sorcerer',
+    wiki: 'https://verum.fandom.com/wiki/Derok_Dranf'
+};
